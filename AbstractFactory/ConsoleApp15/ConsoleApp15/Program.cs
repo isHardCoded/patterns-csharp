dba9fe9a9a1c7b2b42fb130f3ed7d88ce0b53427
isHardCoded <ishardcoded@gmail.com>
2025-02-25 17:03:26 +0500

    Initial commit

---
+++ AbstractFactory/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public interface IMotorcycle
+    {
+        void Ride();
+    }
+
+    public class SportsCar : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Вождение спортивного автомобиля.");
+        }
+    }
+
+    public class SportsMotorcycle : IMotorcycle 
+    {
+        public void Ride()
+        {
+            Console.WriteLine("Вождение спортивного мотоцикла");
+        }
+    }
+
+    public interface IVehicleFactory
+    {
+        ICar CreateCar();
+        IMotorcycle CreateMotorcycle();
+    }
+
+    public class SportsVehicleFactory : IVehicleFactory
+    {
+        public ICar CreateCar() { return new SportsCar(); }
+        public IMotorcycle CreateMotorcycle() { return new SportsMotorcycle(); }
+    }
+
+    public class Client
+    {
+        private ICar _car;
+        private IMotorcycle _motorcycle;
+
+        public Client(IVehicleFactory factory)
+        {
+            _car = factory.CreateCar();
+            _motorcycle = factory.CreateMotorcycle();
+        }
+
+        public void DriveVehicle()
+        {
+            _car.Drive();
+            _motorcycle.Ride();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehicleFactory sportsFactory = new SportsVehicleFactory();
+            Client sportsClient = new Client(sportsFactory);
+            sportsClient.DriveVehicle();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractFactory/ConsoleApp15/ConsoleApp15/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp15")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b30ba67f-dd86-4c9b-b5ac-f74c4d7cbc61")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
